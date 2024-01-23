859d099c973494eede85be91d62c6106733a7b61
Mikad02 <caligiacomo0@gmail.com>
2024-01-23 22:38:03 +0100

    allineato più o meno le cards e aggiunto bottone del next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatIconModule,
+    MatButtonModule,
     MatCardModule,
     MatSelectModule,
     MatSlideToggleModule,

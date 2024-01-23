b5c3d4f5f7f0519e18d4a6d4eb89bb761bb54276
Mikad02 <caligiacomo0@gmail.com>
2024-01-23 21:57:22 +0100

    Card aggiunte con le immagini, ma non cambiano ancora in base all'ingrediente scelto. Ho messo anche le scritte in inglese per coerenza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatCardModule,
     MatSelectModule,
     MatSlideToggleModule,
     BrowserModule,

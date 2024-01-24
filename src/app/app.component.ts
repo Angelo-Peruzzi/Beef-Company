a9afdf875682fadb9351a9a4cc8ea1a210db13d6
Mikad02 <caligiacomo0@gmail.com>
2024-01-24 11:40:36 +0100

    lavorando sui valori del panino con i dropdown e le card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Beef-Company';
+  magicOrder(): void {
+    alert("gay");
+  }
+  
+
+  chosenBread: string = "Nessuna selezione";
+  breadChoice(s: string): void {
+    alert("pane selezionato: " + s);
+    this.chosenBread = s;
+    this.getBread();
+  }
+  getBread(): string {
+    return this.chosenBread;
+  }
+
+  chosenContent: string = "Nessuna selezione";
+  contentChoice(s: string): void {
+    alert("content selezionato: " + s);
+    this.chosenContent = s;
+  }
+  getContent(): string {
+    return this.chosenContent;
+  }
+
+  chosenSauce: string = "Nessuna selezione";
+  sauceChoice(s: string): void {
+    alert("salsa selezionata: " + s);
+    this.chosenSauce = s;
+  }
+  getSauce():string{
+    return this.chosenSauce;
+  }
 }
